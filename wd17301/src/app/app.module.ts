ed557402ae9d77feacc679a76433ce6b67e7ab64
Nguyen Khac Vu <vunkph19573@fpt.edu.vn>
2023-05-27 13:32:54 +0700

    nop bai

--- wd17301/src/app/app.module.ts
+++ wd17301/src/app/app.module.ts
@@ -3,14 +3,39 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout/admin-layout.component';
+import { BaseLayoutComponent } from './layouts/base-layout/base-layout/base-layout.component';
+import { AdminProductComponent } from './pages/admin/admin-product/admin-product/admin-product.component';
+import { DashboardComponent } from './pages/admin/dashboard/dashboard/dashboard.component';
+import { HomePageComponent } from './pages/home-page/home-page/home-page.component';
+import { ProductAddComponent } from './pages/product-add/product-add/product-add.component';
+import { ProductDetailComponent } from './pages/product-detail/product-detail/product-detail.component';
+import { ProductEditComponent } from './pages/product-edit/product-edit/product-edit.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found/page-not-found.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent,
+    AdminLayoutComponent,
+    BaseLayoutComponent,
+    AdminProductComponent,
+    DashboardComponent,
+    HomePageComponent,
+    ProductAddComponent,
+    ProductDetailComponent,
+    ProductEditComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
